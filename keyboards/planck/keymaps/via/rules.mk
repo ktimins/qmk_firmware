3241dbca7d4df9f08517542a14ad74c1feb2dbc4
Kyle Timins <KTimins@lazparking.com>
2022-07-22 10:29:31 -0400

    via stuff

--- keyboards/planck/keymaps/via/rules.mk
+++ keyboards/planck/keymaps/via/rules.mk
@@ -1,5 +1,13 @@
-VIA_ENABLE = yes
-LTO_ENABLE = yes
+VIA_ENABLE 		= yes
+LTO_ENABLE 		= yes
 
 # Not enough USB endpoints
 CONSOLE_ENABLE = no
+BOOTLOADER 		= atmel-dfu
+BOOTMAGIC_ENABLE = yes
+NKRO_ENABLE = yes
+BACKLIGHT_ENABLE = yes
+AUDIO_ENABLE = yes
+
+LAYOUTS = ortho_4x12 planck_mit
+LAYOUTS_HAS_RGB = no
